caf00c1e78449764f37cf34048e50ded4490e25d
shruti patl <shrutipatil11823@gmail.com>
2022-01-18 16:15:40 +0530

    Arrow Function

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-var show =function (Name,Age)
+var add =(a=0,b=0) =>
 {
-    console.log(Name,Age)
-
-}
-
-console.log(show('Anisha','12'))
-
-/*
-var add = function (a = 0, b = 0) 
-{
-    var sum = a + b
+    var sum=a+b
     return sum
-    //console.log(a+b)
 }
 
-console.log(add(10,53))
-*/
+console.log(add (20,30))
