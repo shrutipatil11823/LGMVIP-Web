de32b867aa753e79eda909df8d5a20464281c16e
shruti patl <shrutipatil11823@gmail.com>
2022-01-27 15:39:29 +0530

    Array operations

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
-const a = document.querySelectorAll("input")[0]
-const b =document.querySelectorAll("input")[1]
-const add = document.querySelector("#add")
-const multiply =document.querySelector("#multiply")
-const subtract=document.querySelector("#subtract")
-const division=document.querySelector("#division")
-const resultBox = document.querySelector(".result")
+var arr =[1,2,4,6,7,8]
+arr.push(9)     //To push element at last
+arr.unshift(9)    //To push element at first
+arr.shift()         //to remove first element
 
-const calculator = (event,operation) =>
-{
-  switch (operation)
-  {
-    case "add":
-        resultBox.innerHTML=parseInt(a.value)+parseInt(b.value)
-        break;
-    case "multiply":
-        resultBox.innerHTML=parseInt(a.value)*parseInt(b.value)
-        break;
-    case "subtract":
-        resultBox.innerHTML=parseInt(a.value)-parseInt(b.value)
-        break;
-    case "division":
-        resultBox.innerHTML=parseInt(a.value)/parseInt(b.value)
-        break;
-  }
-}
-add.addEventListener('click',(event) => calculator(event,"add"))
-
-multiply.addEventListener("click",(event) => calculator(event,"multiply"))
-
-subtract.addEventListener("click",(event) => calculator(event,"subtract"))
-
-division.addEventListener("click",(event) => calculator(event,"division"))
+var new_arr = arr.slice[2,5]
+console.log(new_arr)            //To check element of definite indexes
