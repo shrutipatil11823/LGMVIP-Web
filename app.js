c59f3de0ffdde473b88e379e134e5977751adbe2
shruti patl <shrutipatil11823@gmail.com>
2022-01-28 20:28:19 +0530

    Filter

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var arr =[1,2,4,6,7,8]
-arr.push(9)     //To push element at last
-arr.unshift(9)    //To push element at first
-arr.shift()         //to remove first element
+//Filter method
 
-var new_arr = arr.slice[2,5]
-console.log(new_arr)            //To check element of definite indexes
+var arr = [2,3,4,5,6]
+
+arr.filter ((element,index) =>{
+    if(element % 2 ==0)
+    console.log(element,index)
+})
